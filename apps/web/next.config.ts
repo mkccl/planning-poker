f797d1de138844cf8e31514f5d65147cefdccba3
Michael Krause <m.krause@flux-mes.de>
2026-02-06 07:09:51 +0100

    initial commit

---
+++ apps/web/next.config.ts
@@ -0,0 +1,9 @@
+import "@planning-poker-nextjs/env/web";
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  typedRoutes: true,
+  reactCompiler: true,
+};
+
+export default nextConfig;
